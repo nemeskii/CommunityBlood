6edd5d26c883e2cb861f661a083f15cf69cd266d
Ato Kuotsu <kuotsuato59@gmail.com>
2026-08-03 14:33:01 +0530

    Delete MAIN directory

--- MAIN/frontend/src/components/DonorProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export default function DonorProtectedRoute({ children }) {
-  const token = localStorage.getItem("donor_token");
-  if (!token) {
-    return <Navigate to="/donor/login" replace />;
-  }
-  return children;
-}
