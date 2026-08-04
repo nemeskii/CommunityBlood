aae6a462ea1aa27ede629aa4ccf77a1301bac8eb
nemeskii <kuotsuato59@gmail.com>
2026-08-04 08:10:09 +0530

    Run migrations at container startup

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -2,6 +2,7 @@
 cd /app || exit 0
 
 php artisan config:clear || true
+php artisan migrate --force || true
 php artisan config:cache || true
 php artisan route:cache || true
 php artisan view:cache || true
