a02dac12b72f9edb194c3fa92ccab14cafd8ffc0
nemeskii <kuotsuato59@gmail.com>
2026-08-04 08:28:06 +0530

    Temporarily seed admin account

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -3,6 +3,7 @@ cd /app || exit 0
 
 php artisan config:clear || true
 php artisan migrate --force || true
+php artisan db:seed --class=AdminSeeder --force || true
 php artisan config:cache || true
 php artisan route:cache || true
 php artisan view:cache || true
